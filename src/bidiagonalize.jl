43190f00d055aa81264ebf121bb10e0eb4dadcf1
Simon Byrne <simonbyrne@gmail.com>
2016-08-01 11:19:43 +0100

    Update for 0.5, clean up some docs

--- src/bidiagonalize.jl
+++ src/bidiagonalize.jl
@@ -15,20 +15,20 @@ function bidiagonalize_tall!{T}(A::Matrix{T},B::Bidiagonal)
     # upper bidiagonal
 
     for i = 1:n
-        x = slice(A, i:m, i)
+        x = view(A, i:m, i)
         τi = LinAlg.reflector!(x)
         B.dv[i] = real(A[i,i])
-        LinAlg.reflectorApply!(x, τi, slice(A, i:m, i+1:n))
+        LinAlg.reflectorApply!(x, τi, view(A, i:m, i+1:n))
         A[i,i] = τi # store reflector in diagonal coefficient
 
         # for Real, this only needs to be n-2
         # needed for Complex to ensure superdiagonal is Real
         if i <= n-1
-            x = slice(A, i, i+1:n)
+            x = view(A, i, i+1:n)
             conj!(x)
             τi = LinAlg.reflector!(x)
             B.ev[i] = real(A[i,i+1])
-            LinAlg.reflectorApply!(slice(A, i+1:m, i+1:n), x, τi)
+            LinAlg.reflectorApply!(view(A, i+1:m, i+1:n), x, τi)
             A[i,i+1] = τi
         end
     end
@@ -53,16 +53,16 @@ function Base.full{T}(P::PackedUVt{T};thin=true)
     U = eye(T,m,w)
     for i = n:-1:1
         τi = A[i,i]
-        x = slice(A, i:m, i)
-        LinAlg.reflectorApply!(x, τi', slice(U, i:m, i:w))
+        x = view(A, i:m, i)
+        LinAlg.reflectorApply!(x, τi', view(U, i:m, i:w))
     end
 
     # Vt = P_{n_2} ... P_1
     Vt = eye(T,n,n)
     for i = n-1:-1:1
         τi = A[i,i+1]
-        x = slice(A, i, i+1:n)
-        LinAlg.reflectorApply!(slice(Vt, i:n, i+1:n), x, τi')
+        x = view(A, i, i+1:n)
+        LinAlg.reflectorApply!(view(Vt, i:n, i+1:n), x, τi')
     end
     U,Vt
 end
